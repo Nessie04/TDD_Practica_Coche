bf522d5ba314fb31a401ab9ae7eedbcb911da436
Nessie04 <117520011+Nessie04@users.noreply.github.com>
2023-03-21 21:19:23 +0100

    refactorizacion del metodo crear_coche a al_crear_un_coche_su_velocidad_es_cero

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_crear_coche(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero(){
         Coche nuevoCoche = new Coche();
     }
 
