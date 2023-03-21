115b5c93feeb3906d6b69e44869f2a50d1917341
Nessie04 <117520011+Nessie04@users.noreply.github.com>
2023-03-21 21:20:47 +0100

    definicion assertions

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -5,7 +5,7 @@ public class TestCoche {
     @Test
     public void test_al_crear_un_coche_su_velocidad_es_cero(){
         Coche nuevoCoche = new Coche();
-        Assertions.
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
 }
