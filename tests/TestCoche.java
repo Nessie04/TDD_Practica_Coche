bea170383dcb53c18151e51ff34d462651fee57f
Nessie04 <117520011+Nessie04@users.noreply.github.com>
2023-03-22 11:30:38 +0100

    primer metodo refactorizado

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero_ines_llopis(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
